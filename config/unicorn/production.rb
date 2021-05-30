184749a0bb39634bf3b428f7a166df85085f41e6
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-05-30 19:12:15 +0300

    unicorn

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,13 +1,13 @@
-working_directory '/var/www/polyintegration/current'
-pid '/var/www/polyintegration/current/tmp/pids/unicorn.pid'
-stderr_path '/var/www/polyintegration/log/unicorn.log'
-stdout_path '/var/www/polyintegration/log/unicorn.log'
-listen '/tmp/unicorn.polyintegration.sock'
+working_directory '/var/www/coono/current'
+pid '/var/www/coono/current/tmp/pids/unicorn.pid'
+stderr_path '/var/www/coono/log/unicorn.log'
+stdout_path '/var/www/coono/log/unicorn.log'
+listen '/tmp/unicorn.coono.sock'
 worker_processes 2
 timeout 30
 
 before_fork do |server, worker|
-  old_pid = "/var/www/polyintegration/current/tmp/pids/unicorn.pid.oldbin"
+  old_pid = "/var/www/coono/current/tmp/pids/unicorn.pid.oldbin"
   if old_pid != server.pid
     begin
     sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
