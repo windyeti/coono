d610db523c301fa59668d6f26ed3d0a7f65c1f37
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-06-02 18:59:55 +0300

    add create csv ransack-filter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,11 +17,16 @@ class ProductsController < ApplicationController
 
     @search = Product.ransack(@params)
     @search.sorts = 'id desc' if @search.sorts.empty?
+
+    @search_id_by_q = Product.ransack(@params).result.pluck(:id)
+
     @products = @search.result.paginate(page: params[:page], per_page: 100)
+
     if params['otchet_type'] == 'selected'
-      Product.csv_param_selected( params['selected_products'], params['otchet_type'])
-      new_file = "#{Rails.public_path}"+'/ins_detail_selected.csv'
-      send_file new_file, :disposition => 'attachment'
+      Services::CsvSelected.call( params['selected_products'])
+      redirect_to '/product_selected.csv'
+      # new_file = "#{Rails.public_path}"+'/ins_detail_selected.csv'
+      # send_file new_file, :disposition => 'attachment'
     end
   end
 
