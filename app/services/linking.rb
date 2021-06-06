c4423931d75814f195680d63797d56535ca6a47a
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-06-06 12:34:18 +0300

    lining kovcheg

--- app/services/linking.rb
+++ app/services/linking.rb
@@ -1,8 +1,8 @@
 class Services::Linking
   def self.call
     Product.find_each do |product|
-      lit_kom = LitKom.find_by(sku: product.sku)
-      product.update(lit_kom: lit_kom) if lit_kom.present?
+      kovcheg = Kovcheg.find_by(sku: product.sku)
+      product.update(kovcheg: kovcheg) if kovcheg.present?
     end
   end
 end
