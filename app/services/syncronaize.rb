ec0d69ce2e9416df0dc3918d62fc4d8ddb7dab25
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-06-10 12:58:28 +0300

    fix sync

--- app/services/syncronaize.rb
+++ app/services/syncronaize.rb
@@ -9,7 +9,7 @@ class Services::Syncronaize
       min_price = [price_lit_kom, price_kovcheg].reject(&:nil?).min
 
       # какую цену брать: минимальную минус один или из nkamin, у которой один не отнимаем
-      min_price = (price_nkamin.present? && price_nkamin < min_price) || min_price.nil? ? price_nkamin : min_price - 1
+      min_price = min_price.nil? || (price_nkamin.present? && price_nkamin < min_price) ? price_nkamin : min_price - 1
 
       product.price = min_price.present? ? min_price : 0
       # product.quantity = "#{lit_kom&.quantity ? lit_kom.quantity : 0}".to_i
