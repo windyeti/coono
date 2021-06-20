df0e96ac5741c8b2adfdd0aed5deab08e2a84b22
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-06-20 20:07:20 +0300

    fix filter Unsync

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,7 @@ class Product < ApplicationRecord
     ["tmf_id_not_null", "Tmf"],
     ["shulepov_id_not_null", "Shulepov"],
     ["realflame_id_not_null", "Realflame"],
-    ["lit_kom_id_or_kovcheg_id_or_nkamin_id_or_tmf_id_or_shulepov_id_or_realflame_id_null", "Unsync"],
+    ["lit_kom_id_and_kovcheg_id_and_nkamin_id_and_tmf_id_and_shulepov_id_and_realflame_id_null", "Unsync"],
     ["lit_kom_id_or_kovcheg_id_or_nkamin_id_or_tmf_id_or_shulepov_id_or_realflame_id_not_null", "Sync"],
   ]
 
