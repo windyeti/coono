8623cdc6061039f0707d85ae3880f8498aec6749
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-06-21 19:46:27 +0300

    first step implement

--- app/services/create_product_tmf.rb
+++ app/services/create_product_tmf.rb
@@ -42,18 +42,32 @@ class Services::CreateProductTmf
   def self.get_product(product_urls, category_path_name)
     product_urls.each do |product_link|
 
-      p "START <<<< начали собирать данные по продукту #{product_link}"
-      begin
-        doc = rest_client_get(product_link)
-      rescue
-        p "Нет такой страницы #{product_link}"
-        next
+    get_product_data(product_link, category_path_name)
+
+    skus = get_sku(product_link)
+    if skus
+      skus.each do |sku|
+        get_product_data(sku[:product_link], category_path_name)
       end
+    end
+    end
+    p "Total: #{Tmf.count}"
+  end
 
-      fid = doc.at('meta[itemprop="sku"]')['content']
+  def self.get_product_data(product_link, category_path_name, main_product = true)
+    p "START <<<< начали собирать данные по продукту #{product_link}"
+    begin
+      doc = rest_client_get(product_link)
+    rescue
+      p "Нет такой страницы #{product_link}"
+      return
+    end
 
-      tov = Tmf.find_by(fid: fid)
+    fid = doc.at('meta[itemprop="sku"]')['content']
 
+    tov = Tmf.find_by(fid: fid)
+
+    if main_product
       desc = doc.at('.tabs_section .current').inner_html rescue nil
       sdesc = doc.at('.advantage').inner_html rescue nil
 
@@ -63,59 +77,80 @@ class Services::CreateProductTmf
         value = tds[1].text.strip
         "#{name}: #{value}"
       end.join(' --- ')
-
-      sku = fid
-
-      pict = get_pict(doc)
-
-      quantity = nil
-
-      title = doc.at('#pagetitle').text.strip rescue nil
-
+      sku = nil
       price = doc.at('.price i').text.strip.gsub(" ", "") rescue nil
+    else
+      desc = nil
+      sdesc = nil
 
-      link = product_link
-      p4 = category_path_name
-
-      categories = category_path_name.split('/')
-      mtitle = doc.at('title').text.strip rescue nil
-      mdesc = doc.at('meta[name="description"]')['content'] rescue nil
-      mkeyw = doc.at('meta[name="keywords"]')['content'] rescue nil
-
-
-
-      pp data = {
-        fid: fid,
-        sku: sku,
-        title: title,
-        sdesc: sdesc,
-        desc: desc,
-        price: price,
-        oldprice: nil,
-        pict: pict,
-        quantity: quantity,
-        p1: p1,
-        p4: p4,
-        link: link,
-        cat: categories[1],
-        cat1: categories[2],
-        cat2: categories[3],
-        cat3: categories[4],
-        cat4: categories[5],
-        mtitle: mtitle,
-        mdesc: mdesc,
-        mkeyw: mkeyw
-      }
-
-      if tov.present?
-        p '================ UPDATE ================'
-        update_product(tov, data)
-      else
-        create_product(data)
-      end
-      "END <<<< закончили собирать данные на продукт:: #{product_link}"
+      p1 = doc.css('.mod_content .right tr').map do |doc_item|
+        tds = doc_item.css('td')
+        name = tds[0].text.strip
+        value = tds[1].text.strip
+        "#{name}: #{value}"
+      end.join(' --- ')
+      sku = doc.at('.top-part .right').text.strip.gsub("Артикул: ", "") rescue nil
+      price = doc.at('.price p').text.strip.gsub(" ", "") rescue nil
     end
-    p "Total: #{Tmf.count}"
+
+
+    pict = get_pict(doc)
+
+    quantity = nil
+
+    title = doc.at('#pagetitle').text.strip rescue nil
+
+
+    link = product_link
+    p4 = category_path_name
+
+    categories = category_path_name.split('/')
+    mtitle = doc.at('title').text.strip rescue nil
+    mdesc = doc.at('meta[name="description"]')['content'] rescue nil
+    mkeyw = doc.at('meta[name="keywords"]')['content'] rescue nil
+
+    pp data = {
+      fid: fid,
+      sku: sku,
+      title: title,
+      sdesc: sdesc,
+      desc: desc,
+      price: price,
+      oldprice: nil,
+      pict: pict,
+      quantity: quantity,
+      p1: p1,
+      p4: p4,
+      link: link,
+      cat: categories[1],
+      cat1: categories[2],
+      cat2: categories[3],
+      cat3: categories[4],
+      cat4: categories[5],
+      mtitle: mtitle,
+      mdesc: mdesc,
+      mkeyw: mkeyw
+    }
+
+    if tov.present?
+      p '================ UPDATE ================'
+      update_product(tov, data)
+    else
+      create_product(data)
+    end
+    "END <<<< закончили собирать данные на продукт:: #{product_link}"
+  end
+
+  def self.get_sku(product_link)
+    p "START <<<< начали собирать данные по ВАРИАНТУ #{product_link}"
+    begin
+      doc = rest_client_get(product_link)
+    rescue
+      p "Нет такой страницы #{product_link}"
+      return
+    end
+
+
   end
 
   def self.update_product(tov, data)
--- lib/tasks/test.rake
+++ lib/tasks/test.rake
@@ -6,28 +6,37 @@ namespace :p do
   task p: :environment do
     link = 'https://realflame.ru/kaminnye-drovniki/9-10000bk-drovnik'
 
-    doc = get_doc(link)
+    doc = rest_client_get(link)
     p get_pict(doc)
   end
+
   task t: :environment do
 
   end
-  # def get_pict(doc)
-  #   result = []
-  #   doc_picts = doc.css('#thumbs_list_frame a')
-  #   if doc_picts.present?
-  #     result = doc_picts.map do |doc_pict|
-  #       doc_pict['href']
-  #     end
-  #   elsif doc.at('#bigpic')
-  #     result << doc.at('#bigpic')['src']
-  #   end
-  #   result.join(' ')
-  # end
-  #
-  # def get_doc(url)
-  #   category_url = URI.escape(url)
-  #   Nokogiri::HTML(RestClient::Request.execute(:url => category_url, :timeout => 100, :method => :get, :verify_ssl => false))
-  # end
+
+  def get_pict(doc)
+    result = []
+    doc_picts = doc.css('.wrapp_thumbs li')
+    if doc_picts.present?
+      result = doc_picts.map do |doc_pict|
+        "https://t-m-f.ru#{doc_pict['data-big_img']}" if doc_pict['data-big_img'].present?
+      end
+    elsif doc.at('.item_main_info img')
+      result << "https://t-m-f.ru#{doc.at('.item_main_info img')['src']}"
+    else
+      nil
+    end
+    result.join(' ')
+  end
+
+  def get_doc(url)
+    category_url = URI.escape(url)
+    Nokogiri::HTML(RestClient::Request.execute(:url => category_url, :timeout => 100, :method => :get, :verify_ssl => false))
+  end
+
+  def rest_client_get(url)
+    response = RestClient.get(url)
+    Nokogiri::HTML(response.body)
+  end
 
 end
