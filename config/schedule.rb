4c3fcd76b680b7231510a69e29b726ce767dc7b1
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-07-01 11:44:13 +0300

    change schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -32,10 +32,14 @@
 # set :output, "/log/cron.log"
 # set :chronic_options, :hours24 => true
 
-every 1.day, :at => '22:10' do
+every 1.day, :at => '21:30' do
   runner "ProductImportInsalesXmlJob.perform_later"
 end
 
+
+every 1.day, :at => '21:40' do
+  runner "SaunaruJob.perform_later"
+end
 
 every 1.day, :at => '22:20' do
   runner "SawoJob.perform_later"
