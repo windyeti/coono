3e9e3d364232d76b0a4830ce103ce9ddc753fd4a
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-07-16 18:42:07 +0300

    change sycronaize

--- app/services/syncronaize.rb
+++ app/services/syncronaize.rb
@@ -7,16 +7,19 @@ class Services::Syncronaize
       # вычисляем минимальную цену, если price_nkamin отсутствуюет
       # SAWO цена без отнятия 1
       unless price_nkamin
-        price_lit_kom = product.lit_kom.price.to_f - 1 if product.lit_kom.present? && product.lit_kom.price.present?
-        price_kovcheg = product.kovcheg.price.to_f - 1 if product.kovcheg.present? && product.kovcheg.price.present?
-        price_tmf = product.tmf.price.to_f - 1 if product.tmf.present? && product.tmf.price.present?
-        price_shulepov = product.shulepov.price.to_f - 1 if product.shulepov.present? && product.shulepov.price.present?
-        price_realflame = product.realflame.price.to_f - 1 if product.realflame.present? && product.realflame.price.present?
-        price_dim = product.dim.price.to_f - 1 if product.dim.present? && product.dim.price.present?
+
+        delta = !!product.distributor[/Fireway|FireWay/] ? 0 : 1
+
+        price_lit_kom = product.lit_kom.price.to_f - delta if product.lit_kom.present? && product.lit_kom.price.present?
+        price_kovcheg = product.kovcheg.price.to_f - delta if product.kovcheg.present? && product.kovcheg.price.present?
+        price_tmf = product.tmf.price.to_f - delta if product.tmf.present? && product.tmf.price.present?
+        price_shulepov = product.shulepov.price.to_f - delta if product.shulepov.present? && product.shulepov.price.present?
+        price_realflame = product.realflame.price.to_f - delta if product.realflame.present? && product.realflame.price.present?
+        price_dim = product.dim.price.to_f - delta if product.dim.present? && product.dim.price.present?
         price_sawo = product.sawo.price.to_f if product.sawo.present? && product.sawo.price.present?
-        price_saunaru = product.saunaru.price.to_f - 1 if product.saunaru.present? && product.saunaru.price.present?
-        price_teplodar = product.teplodar.price.to_f - 1 if product.teplodar.present? && product.teplodar.price.present?
-        price_contact = product.contact.price.to_f - 1 if product.contact.present? && product.contact.price.present?
+        price_saunaru = product.saunaru.price.to_f - delta if product.saunaru.present? && product.saunaru.price.present?
+        price_teplodar = product.teplodar.price.to_f - delta if product.teplodar.present? && product.teplodar.price.present?
+        price_contact = product.contact.price.to_f - delta if product.contact.present? && product.contact.price.present?
 
         min_price = [price_lit_kom, price_kovcheg, price_tmf, price_shulepov, price_realflame, price_dim, price_sawo, price_saunaru, price_teplodar, price_contact].reject(&:nil?).min
       end
