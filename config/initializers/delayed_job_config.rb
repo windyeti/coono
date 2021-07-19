f40af2335ba39a99866790eeb500378897cbfe21
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-07-20 00:40:24 +0300

    change delaed_job max_run_time

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,7 +1,7 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay = 30
 Delayed::Worker.max_attempts = 1
-Delayed::Worker.max_run_time = 180.minutes
+Delayed::Worker.max_run_time = 1.day
 Delayed::Worker.read_ahead = 10
 Delayed::Worker.default_queue_name = 'default'
 # Delayed::Worker.delay_jobs = !(Rails.env.test? || Rails.env.development?)
