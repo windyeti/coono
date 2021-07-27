14b78d76a000c3e49d62d4470bd92386f5ff6d27
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-07-27 13:38:35 +0300

    linking sawo

--- app/services/linking.rb
+++ app/services/linking.rb
@@ -2,13 +2,17 @@ class Services::Linking
   def self.call
     Product.find_each do |product|
 
-      # Lit-kom
-      lit_kom = LitKom.find_by(sku: product.sku)
-      product.update(lit_kom: lit_kom) if lit_kom.present?
+      # # Lit-kom
+      # lit_kom = LitKom.find_by(sku: product.sku)
+      # product.update(lit_kom: lit_kom) if lit_kom.present?
+      #
+      # # # Kovcheg
+      # kovcheg = Kovcheg.find_by(sku: product.sku)
+      # product.update(kovcheg: kovcheg) if kovcheg.present?
 
-      # # Kovcheg
-      kovcheg = Kovcheg.find_by(sku: product.sku)
-      product.update(kovcheg: kovcheg) if kovcheg.present?
+      # # SAWO
+      sawo = Sawo.find_by(sku: product.sku)
+      product.update(sawo: sawo) if sawo.present?
 
       # # # Tmf
       # !!!! Артикулы не совпадают !!!!
