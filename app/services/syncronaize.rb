61a941a38412d501b7926cd97ba0a18bbb464971
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-08-16 17:30:09 +0300

    change price dim

--- app/services/syncronaize.rb
+++ app/services/syncronaize.rb
@@ -15,7 +15,7 @@ class Services::Syncronaize
         price_tmf = product.tmf.price.to_f - delta if product.tmf.present? && product.tmf.price.present?
         price_shulepov = product.shulepov.price.to_f - delta if product.shulepov.present? && product.shulepov.price.present?
         price_realflame = product.realflame.price.to_f - delta if product.realflame.present? && product.realflame.price.present?
-        price_dim = product.dim.price.to_f - delta if product.dim.present? && product.dim.price.present?
+        price_dim = product.dim.price.to_f if product.dim.present? && product.dim.price.present?
         price_sawo = product.sawo.price.to_f if product.sawo.present? && product.sawo.price.present?
         price_saunaru = product.saunaru.price.to_f - delta if product.saunaru.present? && product.saunaru.price.present?
         price_teplodar = product.teplodar.price.to_f - delta if product.teplodar.present? && product.teplodar.price.present?
