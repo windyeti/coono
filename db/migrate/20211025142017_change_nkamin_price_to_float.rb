67c537782ef164b46a92408a8e45b27da55a9032
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-10-25 17:23:47 +0300

    nkamin change type price

---
+++ db/migrate/20211025142017_change_nkamin_price_to_float.rb
@@ -0,0 +1,5 @@
+class ChangeNkaminPriceToFloat < ActiveRecord::Migration[5.0]
+  def change
+    change_column :nkamins, :price, 'double precision USING CAST(price AS double precision)'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211025134253) do
+ActiveRecord::Schema.define(version: 20211025142017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -402,7 +402,7 @@ ActiveRecord::Schema.define(version: 20211025134253) do
     t.string   "sdesc"
     t.string   "desc"
     t.string   "oldprice"
-    t.string   "price"
+    t.float    "price"
     t.string   "pict"
     t.string   "quantity"
     t.string   "cat"
