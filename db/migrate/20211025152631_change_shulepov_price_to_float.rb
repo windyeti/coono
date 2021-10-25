039ab3e9d16c9e028e61c18dc4ed8b92c2ebf83b
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-10-25 18:35:42 +0300

    shulepoc change price type

---
+++ db/migrate/20211025152631_change_shulepov_price_to_float.rb
@@ -0,0 +1,5 @@
+class ChangeShulepovPriceToFloat < ActiveRecord::Migration[5.0]
+  def change
+    change_column :shulepovs, :price, 'double precision USING CAST(price AS double precision)'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211025150612) do
+ActiveRecord::Schema.define(version: 20211025152631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -617,7 +617,7 @@ ActiveRecord::Schema.define(version: 20211025150612) do
     t.string   "sdesc"
     t.string   "desc"
     t.string   "oldprice"
-    t.string   "price"
+    t.float    "price"
     t.string   "pict"
     t.string   "quantity"
     t.string   "cat"
