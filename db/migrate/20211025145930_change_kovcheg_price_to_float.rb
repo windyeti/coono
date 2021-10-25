847f14114d3799cd79e01d1c05e97cd374320d8c
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-10-25 18:02:57 +0300

    kovcheg change price type

---
+++ db/migrate/20211025145930_change_kovcheg_price_to_float.rb
@@ -0,0 +1,5 @@
+class ChangeKovchegPriceToFloat < ActiveRecord::Migration[5.0]
+  def change
+    change_column :kovchegs, :price, 'double precision USING CAST(price AS double precision)'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211025145058) do
+ActiveRecord::Schema.define(version: 20211025145930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -318,7 +318,7 @@ ActiveRecord::Schema.define(version: 20211025145058) do
     t.string   "sdesc"
     t.string   "desc"
     t.string   "oldprice"
-    t.string   "price"
+    t.float    "price"
     t.string   "pict"
     t.string   "quantity"
     t.string   "cat"
