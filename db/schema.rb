fbe1e06f5a3348ec85c19f49d1b7d27978a124a5
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-10-25 19:26:21 +0300

    dantexgroup change price type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211025161619) do
+ActiveRecord::Schema.define(version: 20211025162331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -219,7 +219,7 @@ ActiveRecord::Schema.define(version: 20211025161619) do
     t.string   "sdesc"
     t.string   "desc"
     t.string   "oldprice"
-    t.string   "price"
+    t.float    "price"
     t.string   "pict"
     t.string   "quantity"
     t.string   "cat"
