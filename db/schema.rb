ce595b124279fdcb54788931b9dd4d240e640c91
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-10-25 19:14:01 +0300

    contact change price type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211025160811) do
+ActiveRecord::Schema.define(version: 20211025161228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -177,7 +177,7 @@ ActiveRecord::Schema.define(version: 20211025160811) do
     t.string   "sdesc"
     t.string   "desc"
     t.string   "oldprice"
-    t.string   "price"
+    t.float    "price"
     t.string   "pict"
     t.string   "quantity"
     t.string   "cat"
